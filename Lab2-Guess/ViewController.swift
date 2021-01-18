93b8d76f565a84505125a75204b695c683d9d1c0
King Chen <king321.chen@google.com>
2021-01-18 20:00:56 +0800

    Initial Commit

---
+++ Lab2-Guess/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Lab2-Guess
+//
+//  Created by cobi321 on 2021/1/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
